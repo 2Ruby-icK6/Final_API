07fb5e8dc5adb9b981d31eaa23c0a461363ea926
2Ruby-icK6 <202180232@psu.palawan.edu.ph>
2023-12-23 00:02:15 +0800

    add complex selection

--- api.py
+++ api.py
@@ -17,15 +17,35 @@ mysql = MySQL(app)
 def index():
     return "<p> Hello World </p>"
 
-@app.route("/client")
-def get_client():
+def data_fetchall(sql_query):
     conn = mysql.connection.cursor()
-    query = "SELECT * FROM client_fees.clients;"
-    conn.execute(query)
+    conn.execute(sql_query)
     data = conn.fetchall()
     conn.close()
 
+    return data
+
+@app.route("/client", methods=["GET"])
+def get_client():
+    query = """SELECT * FROM client_fees.clients;"""
+    data = data_fetchall(query)
+
     return make_response(jsonify(data), 200)
+
+@app.route("/client/<int:id>", methods=["GET"])
+def get_client_by_id(id):
+    query = f"""SELECT * FROM client_fees.clients where client_id = {id};"""
+    data = data_fetchall(query)
+
+    return make_response(jsonify(data), 200)
+
+@app.route("/client/<int:id>/projects", methods=["GET"])
+def get_client_projets(id):
+    query = f"""SELECT projects.project_name, CONCAT(YEAR(project_start_date), " - " ,YEAR(project_end_date)) as project_year FROM clients 
+                INNER JOIN projects on clients.client_id = projects.client_id WHERE clients.client_id = {id};"""
+    data = data_fetchall(query)
+
+    return make_response(jsonify({"client_id": id, "count": len(data), "projects": data}), 200)
 
 if __name__ == "__main__":
     app.run(debug=True)
