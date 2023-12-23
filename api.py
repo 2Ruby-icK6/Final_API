515ce506af3e65564851ca08b8210bd93b0dca03
2Ruby-icK6 <202180232@psu.palawan.edu.ph>
2023-12-23 11:06:39 +0800

    fix bug

--- api.py
+++ api.py
@@ -1,10 +1,9 @@
 from flask import Flask, make_response, jsonify, request, render_template, session
 from flask_mysqldb import MySQL
 import jwt
+from jwt import ExpiredSignatureError, InvalidTokenError
 from datetime import datetime, timedelta
 from functools import wraps
-
-
 
 app = Flask(__name__)
 
@@ -16,7 +15,7 @@ app.config['MYSQL_DB'] = "client_fees"
 
 app.config['MYSQL_CURSORCLASS'] = "DictCursor"
 
-app.config['SECRET_KEY'] = '66aa4d9cdf2c4469bbf9c89472bcf73b'
+app.config['SECRET_KEY'] = '86f342b22484426eb61f220f10f17e48'
 
 mysql = MySQL(app)
 
@@ -27,12 +26,20 @@ def token_required(func):
         if not token:
             return jsonify({'Alert!': 'Token is Missing!'})
         try:
-            payload = jwt.decode(token, app.config['SECRET_KEY'])
-        except:
-            return jsonify({'Alert!': 'Invalid Token!'})
+            payload = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+            kwargs['token_payload'] = payload
+
+        except jwt.ExpiredSignatureError:
+            return jsonify({'Alert!': 'Token has expired!'})
+        except jwt.InvalidTokenError as e:
+            return jsonify({'Alert!': f'Invalid Token: {str(e)}'})
+        except jwt.PyJWTError as e:
+            return jsonify({'Alert!': f'JWT Error: {str(e)}'})
         return func(*args, **kwargs)
-    return decorated
     
+    return decorated
+
+
 @app.route("/")
 def index():
     if not session.get('logged_in'):
@@ -45,10 +52,9 @@ def login():
     if request.form['username'] and request.form['password'] == '12345678':
         session['logged_in'] = True
         token = jwt.encode({
-            'user':request.form['username'],
-            'expiration': str(datetime.utcnow() + timedelta(seconds=120))
-        },
-            app.config['SECRET_KEY'])
+                'user': request.form['username'],
+                'expiration': str(datetime.utcnow() + timedelta(seconds=120))
+            }, app.config['SECRET_KEY'], algorithm='HS256')
         return jsonify({'token': token})
     
     else:
@@ -65,7 +71,7 @@ def data_fetchall(sql_query):
 # Get Endpoints ==========================================================================================================
 @app.route("/client", methods=["GET"])
 @token_required
-def get_client():
+def get_client(token_payload):
     query = """SELECT * FROM client_fees.clients;"""
     data = data_fetchall(query)
 
@@ -73,7 +79,7 @@ def get_client():
 
 @app.route("/client/<int:id>", methods=["GET"])
 @token_required
-def get_client_by_id(id):
+def get_client_by_id(token_payload, id):
     format_type = request.args.get("format", "json")
     query = f"""SELECT * FROM client_fees.clients WHERE client_id = {id};"""
     data = data_fetchall(query)
@@ -88,7 +94,7 @@ def get_client_by_id(id):
 
 @app.route("/client/<int:id>/projects", methods=["GET"])
 @token_required
-def get_client_projets(id):
+def get_client_projets(token_payload, id):
     query = f"""SELECT projects.project_name, CONCAT(YEAR(project_start_date), " - " ,YEAR(project_end_date)) as project_year FROM clients 
                 INNER JOIN projects on clients.client_id = projects.client_id WHERE clients.client_id = {id};"""
     data = data_fetchall(query)
@@ -98,7 +104,7 @@ def get_client_projets(id):
 # http://127.0.0.1:5000/client/search?criteria=pasamonte
 @app.route("/client/search", methods=["GET"])
 @token_required
-def search_clients():
+def search_clients(token_payload):
     criteria = request.args.get("criteria")
     query = f"""SELECT * FROM client_fees.clients WHERE client_name LIKE '%{criteria}%';"""
     data = data_fetchall(query)
@@ -108,7 +114,7 @@ def search_clients():
 # Post Endpoint ==========================================================================================================
 @app.route("/client", methods=["POST"]) 
 @token_required
-def add_client():
+def add_client(token_payload):
     conn = mysql.connection.cursor()
     info = request.get_json()
     client_name = info['client_name']
@@ -130,7 +136,7 @@ def add_client():
 # Put Endpoint ==========================================================================================================
 @app.route("/client/<int:id>", methods=["PUT"])
 @token_required
-def update_client(id):
+def update_client(token_payload, id):
     conn = mysql.connection.cursor()
     info = request.get_json()
     client_name = info['client_name']
@@ -153,7 +159,7 @@ def update_client(id):
 # Delete Endpoint ==========================================================================================================
 @app.route("/client/<int:id>", methods=["DELETE"])
 @token_required
-def delete_client(id):
+def delete_client(token_payload, id):
     conn = mysql.connection.cursor()
     query = f"""DELETE FROM `client_fees`.`clients` WHERE (`client_id` = '{id}');"""
     conn.execute(query)
@@ -161,7 +167,7 @@ def delete_client(id):
     rows_delete = conn.rowcount
     conn.close()
 
-    return make_response(jsonify({"message": "Updated Successfully", "row_deleted": rows_delete}), 200)
+    return make_response(jsonify({"message": "Deleted Successfully", "row_deleted": rows_delete}), 200)
 
 if __name__ == "__main__":
     app.run(debug=True)
